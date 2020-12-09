54d409a7a748e3ceb26ef3f4eeb80ea153d50cfd
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2020-12-08 23:37:13 -0300

    #8; MSG: Criação do gerenciamento de executáveis, com opção de listagem, criação, edição e remoção.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,20 @@ import {CadastroUsuarioComponent} from './components/usuarios/cadastro-usuario/c
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MAT_DATE_LOCALE} from '@angular/material/core';
 import {LoginComponent} from './components/login/login/login.component';
+import {ExecutavelListaComponent} from './components/executavel/executavel-lista/executavel-lista.component';
+import {ExecutavelEdicaoComponent} from './components/executavel/executavel-edicao/executavel-edicao.component';
+import {ExecutavelComponent} from './components/executavel/executavel.component';
+import {ExecutavelInicioComponent} from './components/executavel/executavel-inicio/executavel-inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroUsuarioComponent,
-    LoginComponent
+    LoginComponent,
+    ExecutavelListaComponent,
+    ExecutavelEdicaoComponent,
+    ExecutavelComponent,
+    ExecutavelInicioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/executavel.service.ts
@@ -0,0 +1,71 @@
+import {Injectable} from '@angular/core';
+import {Executavel} from '../models/executavel.model';
+import {Parametro} from '../models/parametro.model';
+import {Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExecutavelService {
+  private executaveis: Executavel[] = [
+    new Executavel(1, 'Gerar relatório de vendas da semana',
+      'Gerar relatório em formato XLSX de vendas da semana e salva na pasta \'Meus documentos\'',
+      'TaskGerarRelatorioSemanal',
+      [
+        new Parametro('nomeRelatório', 'Relatório Vendas Semanal'),
+        new Parametro('dataIniciao', '28/09/2020'),
+      ]),
+    new Executavel(2, 'Atualizar os dados do painel de BI',
+      'Atualizar o painel de BI com os últimos dados produzidos pelo sistema',
+      'TaskAtualizarDadosBI',
+      [
+        new Parametro('nomePainel', 'Painel Analítico de Gastos')
+      ]),
+    new Executavel(3, 'Enviar e-mail parabenizando os aniversariantes do mês',
+      'Envia e-mail de felicitação aos aniversariantes do mês da sua equipe',
+      'TaskEnviarEmailAniversariantesMes',
+      [
+        new Parametro('mes', 'setembro')
+      ])
+  ];
+
+  listaExecutaveisSubject = new Subject<Executavel[]>();
+
+  getExecutaveis(): Executavel[] {
+    return this.executaveis.slice();
+  }
+
+  adicionarExecutavel(executavel: Executavel): void {
+    this.executaveis.push(executavel);
+    this.disparaEventoAtualizacaoListaExecutaveis();
+  }
+
+  removerExecutavel(id: number): void {
+    const posicaoNaLista = this.executaveis.map((executavel: Executavel) => {
+      return executavel.id;
+    }).indexOf(id);
+    this.executaveis.splice(posicaoNaLista, 1);
+    this.disparaEventoAtualizacaoListaExecutaveis();
+  }
+
+  atualizarExecutavel(id: number, executavelAtualizado: Executavel): void {
+    const posicaoNaLista = this.executaveis.map((executavel: Executavel) => {
+      return executavel.id;
+    }).indexOf(id);
+    this.executaveis[posicaoNaLista] = executavelAtualizado;
+    this.disparaEventoAtualizacaoListaExecutaveis();
+  }
+
+  getExecutavelPorId(id: number): Executavel {
+    const executavel = this.executaveis.find(
+      (executavelAtual: Executavel) => {
+        return executavelAtual.id === id;
+      }
+    );
+    return executavel;
+  }
+
+  private disparaEventoAtualizacaoListaExecutaveis(): void {
+    this.listaExecutaveisSubject.next(this.executaveis.slice());
+  }
+}
